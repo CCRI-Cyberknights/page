1addc6ba9f69ad29cb3375b36f2789a1eafabe07
Zachary Hartmann <zackhartmann@gmail.com>
2025-10-07 16:13:55 -0400

    chore(release): 1.8.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.8.6](https://github.com/CCRI-Cyberknights/page/compare/v1.8.5...v1.8.6) (2025-10-07)
+
 ### [1.8.5](https://github.com/CCRI-Cyberknights/page/compare/v1.8.4...v1.8.5) (2025-10-07)
 
 
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // Primary Colors
+        'primary-green': 'var(--primary-green)',     // #04703C
+        'primary-green-light': 'var(--primary-green-light)', // #0A8B4F
+        
+        // Accent Colors
+        'neon': 'var(--neon-surge)',                 // #43CC50
+        'ember': 'var(--ember-spark)',               // #C27329
+        'arc': 'var(--arc-weld-blue)',               // #2DB2C4
+        'molten': 'var(--molten-glow)',              // #FFCC33
+        
+        // Neutral Colors
+        'forge': 'var(--forge-black)',                // #001011
+        'iron': 'var(--iron-ash)',                   // #1C1C1C
+        'steel': 'var(--steel-glow)',                // #5B6E6E
+        'silver': 'var(--silver-edge)',              // #8A8A8A
+        'alloy': 'var(--pale-alloy)',                // #B8B8B8
+      },
+    },
+  },
+}
